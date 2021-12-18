32a12e1098a67b3db0c98ec611ca6e1bed004b2e
PlaZZma <PlaZZma@DESKTOP-FSCRLD6>
2021-12-18 09:42:31 +0200

    Добавьте файлы проекта.

---
+++ MVVMsteps/Model/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMsteps.Model
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName[0]}. {LastName}: {PhoneNumber}";
+
+        }
+    }
+}
