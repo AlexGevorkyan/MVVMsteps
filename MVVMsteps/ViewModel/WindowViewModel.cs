d034b5012ea8e5f2b7912422ee2b5e205e91bc7e
Alex <gag63a@gmail.com>
2021-12-18 20:37:05 +0200

    step 2

--- MVVMsteps/ViewModel/WindowViewModel.cs
+++ MVVMsteps/ViewModel/WindowViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using MVVMsteps.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace MVVMsteps.ViewModel
 {
-    public class WindowViewModel //: ViewModelBase
+    public class WindowViewModel : INotifyPropertyChanged
     {
         //Contact contact = new Contact { FirstName="Clifford", LastName="Simak", PhoneNumber="111"};
         //public string LastName 
@@ -13,6 +14,25 @@ namespace MVVMsteps.ViewModel
         //    get { return contact.LastName; }
         //    set { contact.LastName = value;  }
         //}
-        public string LastName { set; get; }
+        //public string LastName { set; get; }
+
+        string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+
     }
 }
